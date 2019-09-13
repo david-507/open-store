9bf1d18757035a5284f2dab29ebbd8efdbf840ca
David <david.507@yahoo.com>
2019-09-13 14:18:40 +0200

    Updated: - list products - edit and delete product views - add Delete products method on Controller

---
+++ src/main/java/org/dmace/store/controller/admin/AdminController.java
@@ -0,0 +1,55 @@
+package org.dmace.store.controller.admin;
+
+import org.dmace.store.service.CategoryService;
+import org.dmace.store.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private final Logger logger = LoggerFactory.getLogger(AdminController.class);
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private ProductService productservice;
+
+    @GetMapping("/categories")
+    public String categories(Model model) {
+        model.addAttribute("categories", categoryService.findAll());
+
+        return "admin/categories";
+    }
+
+    @GetMapping("/products")
+    public String products(Model model){
+        model.addAttribute("categories", categoryService.findAll());
+        model.addAttribute("products", productservice.findAll());
+        return "admin/products";
+    }
+
+    @DeleteMapping("/products")
+    public ResponseEntity delete(@RequestBody List<Long> ids){
+        logger.info("Deleting products " + ids);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // productService.delete(ids);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+}
--- src/main/java/org/dmace/store/controller/admin/AdminProductController.java
+++ src/main/java/org/dmace/store/controller/admin/AdminProductController.java
@@ -1,16 +1,18 @@
-package org.dmace.store.controller;
+package org.dmace.store.controller.admin;
 
+import org.dmace.store.model.Producto;
 import org.dmace.store.service.CategoryService;
 import org.dmace.store.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin")
-public class AdminController {
+@RequestMapping("/admin/product")
+public class AdminProductController {
 
     @Autowired
     private CategoryService categoryService;
@@ -18,11 +20,10 @@ public class AdminController {
     @Autowired
     private ProductService productservice;
 
-    @GetMapping("/categories")
-    public String categories(Model model) {
+    @GetMapping("/new")
+    public String categories(@ModelAttribute("product") Producto product, Model model) {
         model.addAttribute("categories", categoryService.findAll());
-
-        return "admin/categories";
+        return "/admin/product/new-product";
     }
 
     @GetMapping("/products")
